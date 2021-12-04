9a99e25f50f7899ab7b08bae5001d6fe970fcbe1
Hoang Phan <phanhoangdl@gmail.com>
2021-12-04 10:22:42 +0700

    add config example

---
+++ phone/scripts/SIP.js/config_example.js
@@ -0,0 +1,12 @@
+const user = {
+    //  User Name
+    "User": "",
+    //  Password
+    "Pass": "",
+    //  Auth Realm
+    "Realm": "",
+    // Display Name
+    "Display": "",
+    // WebSocket URL
+    "WSServer": ""
+};
